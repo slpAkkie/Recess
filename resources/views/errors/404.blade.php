92cac79d250c0386a7ff50520c3cac6d986efd41
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-05 13:34:46 +0300

    Add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('tpl.main')
+
+@section('body')
+    <section class="site-section">
+        <div class="container">
+            <div class="row">
+                <h2 class="heading-39291 col-12">Страница не найдена</h2>
+            </div>
+        </div>
+    </section>
+@endsection
