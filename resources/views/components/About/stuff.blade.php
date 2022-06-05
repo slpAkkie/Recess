3abd20205afff86c081420a77f2c6d1e1b11305a
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-06-05 14:41:17 +0300

    Fix styles for stuff card

--- resources/views/components/About/stuff.blade.php
+++ resources/views/components/About/stuff.blade.php
@@ -1,7 +1,11 @@
 <div class="col-lg-4 col-md-6 mb-5">
     <div class="post-entry-1 h-100 bg-white text-center">
         <a href="{{ route('stuff.show', $stuff->id) }}" class="d-inline-block">
-            <img src="{{ asset($stuff->avatar_path) }}" alt="Image" width="260px" height="260px" class="img-fluid stuff-image-00000">
+            @if ($stuff->avatar_path)
+                <img src="{{ asset($stuff->avatar_path) }}" alt="Image" width="260px" height="260px" class="img-fluid stuff-image-00000">
+            @else
+                <div class="stuff-card-preview-bg"></div>
+            @endif
         </a>
         <div class="post-entry-1-contents">
             <span class="meta">{{ $stuff->position }}</span>
